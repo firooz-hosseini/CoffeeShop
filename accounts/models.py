ae5e76ba38185775f8069ca00d655dc890faa22d
sina rezaie <sina.rz1384@gmail.com>
2025-07-29 08:11:42 +0330

    assign_user_favorites

--- accounts/models.py
+++ accounts/models.py
@@ -2,29 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
 
-class CustomUserManger(BaseUserManager):
+class CustomUser(AbstractUser):
 
-    def create_user(self,mobile,password,**extra_field):
+    phone = models.CharField(max_length=11,unique=True)
+    profile_picture = models.ImageField(upload_to='user_picture/',null=True,blank=True)
 
               
-        user = self.model(mobile=mobile, **extra_field)
-        user.set_password(password)
-        user.save()
-        return user
-    
-    def create_superuser(self,mobile , password,  **extra_fields):
-
-        extra_fields.setdefault("is_staff", True) 
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-        return self.create_user(mobile, password, **extra_fields) 
-    
-
-
-
-class CustomUser(AbstractUser):
-    mobile = models.CharField(max_length=11,unique=True)
-    profile_picture = models.ImageField(upload_to='user_picture/',null=True,blank=True)
-    USERNAME_FIELD = 'mobile'
-    REQUIRED_FIELDS = ['email']
-    objects = CustomUserManger()
--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404 , redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product,Favorite
 
-# Create your views here.
+
+@login_required
+def ad_to_favorites(request,prdoct_id):
+    product = get_object_or_404(Product,id = prdoct_id)
+    Favorite.objects.create(user = request.user,product = product)
+    return redirect('product_detail',prdoct_id=product.id)
+
+@login_required
+def user_favorites(request):
+    favorite = Favorite.objects.filter(user = request.user)
+    return render(request,'products/user_favorites.html',{'favorite':favorite})
+
+
+
