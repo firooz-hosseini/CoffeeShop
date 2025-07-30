77d6f178f31e4425aa71ca6bbe1af28c6f85bfea
Firooz <firo744@gmail.com>
2025-07-30 14:36:52 +0330

    Add home page (not complete)

--- CoffeeShop/urls.py
+++ CoffeeShop/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.HomePage.as_view(), name='home'),
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
     path('products/', include('products.urls')),
