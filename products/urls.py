3cadae47abda246608e0349462d121067eebda0c
Firooz <firo744@gmail.com>
2025-07-30 11:17:37 +0330

    fix names

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('favorite/add/<int:product_id>/',views.ad_to_favorites,name='ad_to_favorites'),
+    path('favorite/add/<int:product_id>/',views.add_to_favorites,name='add_to_favorites'),
     path('favorites/',views.user_favorites,name='user_favorites')
 ]
