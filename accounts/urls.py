9a76ee153e797db64d0335f479eba46cec7c7303
Firooz <firo744@gmail.com>
2025-07-30 09:16:39 +0330

    minor improvement

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/',views.MyLoginView.as_view() , name='login'),
     path('profile/', views.profile, name='profile'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-        path('password-reset/', views.MyPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/', views.MyPasswordResetView.as_view(), name='password_reset'),
     path('password-reset/done/', views.MyPasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', views.MyPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', views.MyPasswordResetCompleteView.as_view(), name='password_reset_complete'),
