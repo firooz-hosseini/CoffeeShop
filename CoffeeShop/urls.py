684198fc07fe060756ead74150fc25616bbf7eae
sina rezaie <sina.rz1384@gmail.com>
2025-07-31 07:21:35 +0330

    product/orders/

--- CoffeeShop/urls.py
+++ CoffeeShop/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('orders/', include('orders.urls')),
+    path('product/', include('product.urls')),
 ]
