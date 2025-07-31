c3838677b11f21ea3d147425dc1e984592131dec
Firooz <firo744@gmail.com>
2025-08-01 01:23:50 +0330

    Add home page view

--- CoffeeShop/urls.py
+++ CoffeeShop/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import HomePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
     path('products/', include('products.urls')),
+    path('', HomePageView.as_view(), name='home'),
 ]
