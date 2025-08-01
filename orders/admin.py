6a1fb93542acf3e5954833fcda06bd197eb0a1b6
Firooz <firo744@gmail.com>
2025-08-01 15:01:02 +0330

    Add minor improvement

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import *
-from django.contrib import admin
 from .models import Order, OrderItem, Rating, Comment
 
 @admin.register(Order)
--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import *
-admin.site.register(Product)
+from .models import Product, Category, Ingredient, Favorite, Image
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_filter = ('category', 'ingredient')  
+
 admin.site.register(Category)
 admin.site.register(Ingredient)
 admin.site.register(Favorite)
