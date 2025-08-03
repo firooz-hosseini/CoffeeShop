1bc8ab2192d0dcca4f5fb82e1f56f91c2cf1e21b
sina rezaie <sina.rz1384@gmail.com>
2025-08-03 21:59:08 +0330

    urls_root

--- CoffeeShop/urls.py
+++ CoffeeShop/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import HomePageView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,4 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('', HomePageView.as_view(), name='home'),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
