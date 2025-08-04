78c45c4a2c5a196db21e282ea19842038b520a61
sina rezaie <sina.rz1384@gmail.com>
2025-08-04 18:40:15 +0330

    next page url

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
-    path('login/',views.MyLoginView.as_view() , name='login'),
+    path('login/',views.MyLoginView.as_view(next_page='home') , name='login'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
     path('password-reset/', views.MyPasswordResetView.as_view(), name='password_reset'),
     path('password-reset/done/', views.MyPasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', views.MyPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
