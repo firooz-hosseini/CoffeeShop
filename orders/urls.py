60e9aa25dd028f3d428a9d78fe7a900c42a62fbc
Firooz Hosseini <72876079+firooz-hosseini@users.noreply.github.com>
2025-08-06 00:32:47 +0330

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -3,7 +3,7 @@ from .views import CreateOrderView
 from . import views
 
 urlpatterns = [
-    path('order/create/<int:product_id>/', CreateOrderView.as_view(), name='create_order'),
-    path('order/success/', views.order_success, name='order_success'),
-    path('order/my-orders/', views.OrderListView.as_view(), name='order_list'),
+    path('create/<int:product_id>/', CreateOrderView.as_view(), name='create_order'),
+    path('success/', views.order_success, name='order_success'),
+    path('my-orders/', views.OrderListView.as_view(), name='order_list'),
 ]
