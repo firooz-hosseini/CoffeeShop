556b8061034d48d444a50b5ed252ee47372f2e89
sina rezaie <sina.rz1384@gmail.com>
2025-08-06 16:07:24 +0330

    make view remove and add to favorite

--- CoffeeShop/views.py
+++ CoffeeShop/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-from products.models import Product,Category
+from products.models import Product,Category,Favorite
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
@@ -11,4 +11,9 @@ class HomePageView(TemplateView):
             products = products.filter(category_id=category_id)
         context['products'] = products
         context['categories'] = Category.objects.all()
+        if self.request.user.is_authenticated:
+            favorite_ids = Favorite.objects.filter(user=self.request.user).values_list('product_id', flat=True)
+            context['favorite_product_ids'] = list(favorite_ids)
+        else:
+            context['favorite_product_ids'] = []
         return context
