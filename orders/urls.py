2e440b32ba9e35fcd6f9a439a36e1a55780d8245
erfaneh-eghbali <erfaneh.eghbali@gmail.com>
2025-08-06 18:08:08 +0330

    status of order

--- orders/urls.py
+++ orders/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('create/<int:product_id>/', CreateOrderView.as_view(), name='create_order'),
     path('success/', views.order_success, name='order_success'),
     path('my-orders/', views.OrderListView.as_view(), name='order_list'),
+    path('canceled/<int:order_id>', views.cancel_order_view, name='canceled_order'),
+    path('delivered/<int:order_id>', views.deliver_order_view, name='delivered_order')
 ]
