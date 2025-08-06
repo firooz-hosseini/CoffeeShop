7f034403283c1cfd534f5c04d3fe4c22106dbf93
sina rezaie <sina.rz1384@gmail.com>
2025-08-06 15:40:26 +0330

    add view for filter

--- CoffeeShop/views.py
+++ CoffeeShop/views.py
@@ -1,9 +1,14 @@
 from django.views.generic import TemplateView
-from products.models import Product
+from products.models import Product,Category
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['products'] = Product.objects.all()
+        category_id = self.request.GET.get('category')
+        products = Product.objects.all()
+        if category_id:
+            products = products.filter(category_id=category_id)
+        context['products'] = products
+        context['categories'] = Category.objects.all()
         return context
