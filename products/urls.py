699189aa59fb0ab182173ef90c82ed8077bb8a7e
firo74 <firo744@gmail.com>
2025-08-11 12:42:41 +0330

    Remove buttons from home page and add them to product detail view and other changes

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('favorite/add/<int:product_id>/',views.add_to_favorites,name='add-to-favorites'),
     path('favorites/',views.user_favorites,name='user_favorites'),
     path('remove-favorites/<int:pk>/',views.remove_from_favorites,name='remove-favorites'),
-    path('product-list/', views.ProductListView.as_view(), name='product-list'),
-    path('product-detail/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'), 
+    path('', views.ProductListView.as_view(), name='product-list'),
+    path('<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'), 
 ]
