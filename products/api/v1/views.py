4207b4b0a935747a37d82ad1076615a0e34729de
firo74 <firo744@gmail.com>
2025-08-21 00:47:53 +0330

    Fix errors of viewset

--- products/api/v1/views.py
+++ products/api/v1/views.py
@@ -1,10 +1,10 @@
-from rest_framework import views, permissions
+from rest_framework import viewsets, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from products.models import Product, Category, Favorite
 from .serializers import ProductSerializer
 
-class ProductViewSet(views.ModelViewSet):
+class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'id'
