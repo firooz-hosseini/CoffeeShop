0f1bce4f061afe86a030e40d8eabc3ecc21bfb87
sina rezaie <sina.rz1384@gmail.com>
2025-08-20 22:38:59 +0330

    add filter search ordering

--- products/api/v1/viewes.py
+++ products/api/v1/viewes.py
@@ -2,13 +2,17 @@ from rest_framework import viewsets, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from products.models import Product, Category, Favorite
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer,CategorySerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'id'
-    
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ["category"]
+    search_fields = ["name", "description", "tags"]
+    ordering_fields = ["price", "name", "stock"]
+    ordering = ["id"]
     def get_permissions(self):
         if self.action == 'create':
             permission_classes = [permissions.IsAdminUser]
@@ -21,4 +25,5 @@ class ProductViewSet(viewsets.ModelViewSet):
         
         else:
             permission_classes = [permissions.AllowAny]
-        return [permission() for permission in permission_classes]    
+        return [permission() for permission in permission_classes]
+  
