e7460a26edaa43222571766eeefc9cb6bf8b7755
sina rezaie <sina.rz1384@gmail.com>
2025-08-20 22:35:59 +0330

    add categorySerializer

--- products/api/v1/serializers.py
+++ products/api/v1/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
-from products.models import Product,Ingredient
+from products.models import Product,Ingredient,Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "title"]
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    ingredient = serializers.StringRelatedField(many=True)
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), source="category", write_only=True)
     class Meta:
         model = Product
         fields = '__all__'
-        ingredient= serializers.StringRelatedField(many=True)
