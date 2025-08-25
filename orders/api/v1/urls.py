a249b564acd16e665cd744c4506dacd8a9be76e3
sina rezaie <sina.rz1384@gmail.com>
2025-08-25 23:22:09 +0330

    add url for Notification

--- orders/api/v1/urls.py
+++ orders/api/v1/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import OrderViewSet,CommentViewSet,RatingViewSet
+from .views import OrderViewSet,CommentViewSet,RatingViewSet,NotificationViewSet
 
 router = DefaultRouter()
 router.register('order',OrderViewSet,basename='order')
 router.register('comment',CommentViewSet,basename='comment')
 router.register('rating',RatingViewSet,basename='rating')
+router.register('notification',NotificationViewSet,basename='rating')
 
 urlpatterns = [router.urls]
