f528e64da1d35419c302dfd65751062a793f71a4
erfaneh-eghbali <erfaneh.eghbali@gmail.com>
2025-08-25 23:26:30 +0330

    minor improvement

--- orders/admin.py
+++ orders/admin.py
@@ -11,17 +11,12 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-
-
     list_display = ['user', 'time', 'status', 'seen_by_admin']
     list_filter = ['time', 'items__product__category', 'status', 'seen_by_admin']
     search_fields = ['user__first_name', 'user__last_name', 'user__username']
     list_editable = ['status']
     
     inlines = [OrderItemInline]
-
-
-
 
     def changelist_view(self, request, extra_context=None):
         new_orders = Order.objects.filter(seen_by_admin=False)
@@ -36,7 +31,6 @@ class OrderAdmin(admin.ModelAdmin):
         obj.seen_by_admin = True
         super().save_model(request, obj, form, change)
     
-
 
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
@@ -76,4 +70,3 @@ class NotificationAdmin(admin.ModelAdmin):
     search_fields = ('message',)
     ordering = ('-created_at',)
     actions = [mark_as_read]
-
