e4a6835e0209da97d9cc3046778fad1b807e5565
sina rezaie <sina.rz1384@gmail.com>
2025-08-25 23:27:13 +0330

    fix error

--- orders/api/v1/urls.py
+++ orders/api/v1/urls.py
@@ -1,5 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from .views import OrderViewSet,CommentViewSet,RatingViewSet,NotificationViewSet
+from django.urls import path,include
 
 router = DefaultRouter()
 router.register('order',OrderViewSet,basename='order')
@@ -7,4 +8,6 @@ router.register('comment',CommentViewSet,basename='comment')
 router.register('rating',RatingViewSet,basename='rating')
 router.register('notification',NotificationViewSet,basename='notification')
 
-urlpatterns = [router.urls]
+urlpatterns = [
+    path('', include(router.urls)),
+]
