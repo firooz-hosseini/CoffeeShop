f6f709c6c6ffd24afdfd105cb3a3ab235b2377ac
sina rezaie <sina.rz1384@gmail.com>
2025-08-25 21:45:02 +0330

    add order item serializer

--- orders/api/v1/serializers.py
+++ orders/api/v1/serializers.py
@@ -1,1 +1,12 @@
+from rest_framework import serializers
+from orders.models import Order, OrderItem, Comment, Rating, Notification
+from products.models import Product
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_title = serializers.ReadOnlyField(source="product.title")
+    product_price = serializers.ReadOnlyField(source="product.price")
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'product_title', 'product_price', 'quantity']
 
