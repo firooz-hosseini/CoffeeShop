ccded84f2f3aaac806707443c6970ff605050c77
sina rezaie <sina.rz1384@gmail.com>
2025-08-25 22:30:36 +0330

    add commentViewSet

--- orders/api/v1/views.py
+++ orders/api/v1/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets,permissions
-from .serializers import OrderSerializer
-from orders.models import Order,Notification
+from .serializers import OrderSerializer,CommentSerializer
+from orders.models import Order,Notification,Comment
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -18,3 +18,13 @@ class OrderViewSet(viewsets.ModelViewSet):
         for admin in admins:
             Notification.objects.create(message=f"new order by{self.request.user.first_name}")
         return order
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
