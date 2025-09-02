e3b51da8dbe6fe263960bc38dcaa00a71efc6485
Firooz Hosseini <firo744@gmail.com>
2025-09-02 23:33:20 +0330

    Add final order model

--- orders/models.py
+++ orders/models.py
@@ -84,3 +84,32 @@ class Notification(models.Model):
     
     def __str__(self):
         return f"Notification: {self.message[:50]}"
+    
+
+
+class FinalOrder(models.Model):
+    STATUS_CHOICES = [
+        ('paid', 'Paid'),
+        ('delivered', 'Delivered'),
+        ('canceled', 'Canceled'),
+    ]
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='final_orders')
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='paid')
+
+    @property
+    def total_price(self):
+        return sum(item.product.price * item.quantity for item in self.items.all())
+
+    def __str__(self):
+        return f"FinalOrder {self.id} - {self.user.mobile}"
+
+
+class FinalOrderItem(models.Model):
+    final_order = models.ForeignKey(FinalOrder, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.product.title} x {self.quantity}"
